ba3a4a5d58cf08f6af0bda6fb9b2bfc5f558a71d
eferhatg <eferhatg@gmail.com>
2018-10-14 20:46:34 +0300

    before format

--- common_problems/pyramid.js
+++ common_problems/pyramid.js
@@ -1,28 +1,23 @@
-export const pyramid=(n)=>{
-  let pyrStr='';
+export const pyramid = (n) => {
+  let pyrStr = '';
 
-  if(n==1) return '#';
+  if (n == 1) return '#';
 
-  var rowCount=n;
-  var colCount=(n-1)+n;
+  let rowCount = n;
+  let colCount = (n - 1) + n;
 
 
-  
   for (let i = 0; i < rowCount; i++) {
-      let row=Array(colCount).fill(' ');
+    const row = Array(colCount).fill(' ');
 
-      let middle=Math.floor(colCount/2);
- 
-      let startIndex=middle-i;
-      let endIndex=middle+i;
-      for(let j=startIndex;j<=endIndex;j++){
-        row[j]='#';
-      }
-      pyrStr+=row.join('')+'\n';
+    const middle = Math.floor(colCount / 2);
 
+    const startIndex = middle - i;
+    const endIndex = middle + i;
+    for (let j = startIndex; j <= endIndex; j++) {
+      row[j] = '#';
+    }
+    pyrStr += `${row.join('')}\n`;
   }
   return pyrStr;
-
-  
-  
-}  
+};
--- data_structures/tree.js
+++ data_structures/tree.js
@@ -1,72 +1,66 @@
 export class Node {
-  constructor(data){
-    this.data=data;
-    this.children=[];
+  constructor(data) {
+    this.data = data;
+    this.children = [];
   }
 
-  add(data){
+  add(data) {
     this.children.push(new Node(data));
     return this;
   }
 
-  remove(data){
-    var dataIx=-1;
-    for(let i=0;i<this.children.length;i++){
-      if(this.children[i].data===data){
-        dataIx=i;
+  remove(data) {
+    let dataIx = -1;
+    for (let i = 0; i < this.children.length; i++) {
+      if (this.children[i].data === data) {
+        dataIx = i;
       }
     }
-    if(dataIx!==-1){
-      this.children.splice(dataIx,1);
+    if (dataIx !== -1) {
+      this.children.splice(dataIx, 1);
     }
-    
   }
-
 }
 
 export class Tree {
-  constructor(){
-    this.root=null;
+  constructor() {
+    this.root = null;
   }
 
-  traverseBF(fn){
-    if(!this.root) return null;
-    var arr=[this.root];
-    while(arr.length){
-        const node= arr.shift();
-        fn(node);
-        arr.push(...node.children);
+  traverseBF(fn) {
+    if (!this.root) return null;
+    let arr = [this.root];
+    while (arr.length) {
+      const node = arr.shift();
+      fn(node);
+      arr.push(...node.children);
     }
   }
 
-  traverseDF(fn){
-    if(!this.root) return null;
-    var arr=[this.root];
-    while(arr.length){
-        const node= arr.pop();
-      
-        arr.push(...node.children);
-        fn(node);
+  traverseDF(fn) {
+    if (!this.root) return null;
+    let arr = [this.root];
+    while (arr.length) {
+      const node = arr.pop();
+
+      arr.push(...node.children);
+      fn(node);
     }
   }
-  
-  levelWidth(){
-    var levelWidth=[0];
-    var arr=[this.root,'s'];
-    while(arr.length>1){
-      var node=arr.shift();
-      if(node==='s'){
-        levelWidth.push(0);      
+
+  levelWidth() {
+    let levelWidth = [0];
+    let arr = [this.root, 's'];
+    while (arr.length > 1) {
+      let node = arr.shift();
+      if (node === 's') {
+        levelWidth.push(0);
         arr.push('s');
-      }else{
+      }else {
         arr.push(...node.children);
-        levelWidth[levelWidth.length-1]++;
+        levelWidth[levelWidth.length - 1]++;
       }
-    
-
     }
     return levelWidth;
   }
-
-
 }
